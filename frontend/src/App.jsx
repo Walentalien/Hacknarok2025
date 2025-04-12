3ea1b867ba25a0f56152fdc91240d35ca0b1b0cf
Grzegorz <grzegorz@static.qsghosting.com>
2025-04-12 21:38:47 +0200

    minor changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,9 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import Home from './pages/Home'
-import Login from './pages/Login'
-import Search from './pages/Search'
-import Profile from './pages/Profile'
-import Discussion from './pages/Discussion'
-import Layout from './components/Layout'
-import './App.css'
-import Landing from './pages/Landing'
 
 function App() {
+
+  // todo
   const isAuthenticated = () => {
     return localStorage.getItem('token') !== null;
   };
@@ -20,30 +15,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/search" element={<Search />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/home" element={
-          <PrivateRoute>
-            <Layout>
-              <Home />
-            </Layout>
-          </PrivateRoute>
-        } />
-        <Route path="/profile" element={
-          <PrivateRoute>
-            <Layout>
-              <Profile />
-            </Layout>
-          </PrivateRoute>
-        } />
-        <Route path="/discussion/:id" element={
-          <PrivateRoute>
-            <Layout>
-              <Discussion />
-            </Layout>
-          </PrivateRoute>
-        } />
+        <Route path="/" element={<Home />} />
       </Routes>
     </Router>
   )
