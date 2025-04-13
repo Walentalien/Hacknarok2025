d80464628f48c06664dab4f8a522e713eeb25848
Walentalien <voliinyk@student.agh.edu.pl>
2025-04-13 07:28:14 +0200

    Thanks to higher powers it still worked here

--- frontend/src/pages/layout.jsx
+++ frontend/src/pages/layout.jsx
@@ -3,10 +3,10 @@ import { Outlet } from 'react-router-dom';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 
-export default function Layout({ searchOpen }) {
+export default function Layout() {
   return (
     <div className="min-h-screen flex flex-col">
-      <Navbar searchOpen = {searchOpen} />
+      <Navbar />
       <main className="flex-1 w-full max-w-7xl mx-auto px-4 mt-10">
         <Outlet />
       </main>
