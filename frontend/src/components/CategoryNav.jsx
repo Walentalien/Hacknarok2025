0c86a139e99d2c24daf8c78b0543227a56543130
Grzegorz <grzegorz@static.qsghosting.com>
2025-04-13 02:27:30 +0200

    recent development raport

--- frontend/src/components/CategoryNav.jsx
+++ frontend/src/components/CategoryNav.jsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import './CategoryNav.css';
-
 const CategoryNav = () => {
   const categories = [
     'W mojej okolicy',
@@ -11,9 +9,9 @@ const CategoryNav = () => {
   ];
 
   return (
-    <nav className="category-nav">
+    <nav className="flex gap-4 py-4 overflow-x-auto">
       {categories.map((category, index) => (
-        <button key={index} className="category-button">
+        <button key={index} className="p-10 px-6 bg-[#f8f9fa] border border-solid border-[e0e0e0] rounded-sm text-[#0B0B66] font-medium whitespace-nowrap cursor-pointer duration-200 ease-in transition-all hover:bg-[#0B0B66] hover:text-white hover:border hover:border-[#0B0B66]">
           {category}
         </button>
       ))}
