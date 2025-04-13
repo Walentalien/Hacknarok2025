4ad640dbad73645e7cb94d5a25a9b697f1fc62c2
Grzegorz <grzegorz@static.qsghosting.com>
2025-04-13 08:32:34 +0200

    check this out

---
+++ frontend/src/pages/chat.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react';
+
+export default function Chat() {
+  const [name, setName] = useState('Taylor');
+  const [age, setAge] = useState(42);
+
+  function handleNameChange(e) {
+    setName(e.target.value);
+  }
+
+  function handleAgeChange() {
+    setAge(age + 1);
+  }
+
+  return (
+    <>
+      <input
+        value={name}
+        onChange={handleNameChange}
+      />
+      <button onClick={handleAgeChange}>
+        Increment age
+      </button>
+      <p>Hello, {name}. You are {age}.</p>
+    </>
+  )
+}
