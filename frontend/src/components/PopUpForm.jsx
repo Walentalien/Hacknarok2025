bf42bfb8b1ae7343c7c5d9c40a9e6fc343092502
Wpawlina <165510437+Wpawlina@users.noreply.github.com>
2025-04-13 11:09:51 +0200

    final fix

--- frontend/src/components/PopUpForm.jsx
+++ frontend/src/components/PopUpForm.jsx
@@ -20,7 +20,7 @@ const PopupForm = () => {
         onClick={() => setIsOpen(true)}
         className="bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded"
       >
-        Otwórz popup
+        Dodaj wątek
       </button>
 
       {/* Popup */}
