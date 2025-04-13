829bbba58412c8dcd2d1a5a67627a3da6edcdf6c
Walentalien <voliinyk@student.agh.edu.pl>
2025-04-13 02:38:56 +0200

    ss

--- frontend/src/pages/layout.jsx
+++ frontend/src/pages/layout.jsx
@@ -1,6 +1,6 @@
 // Layout.jsx
 import { Outlet } from 'react-router-dom';
-import Navbar from '../components/Navbar';
+import Navbar from '../components/navbar';
 import Footer from '../components/footer';
 
 export default function Layout() {
